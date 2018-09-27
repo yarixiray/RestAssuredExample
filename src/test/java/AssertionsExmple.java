1e7807c197b32f535790a04be8cef6e23ee429d6
iaroslav.liabogov <iaroslav.liabogov@globallogic.com>
2018-09-27 12:16:25 +0200

    upd

--- src/test/java/AssertionsExmple.java
+++ src/test/java/AssertionsExmple.java
@@ -5,6 +5,9 @@ import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
 
 public class AssertionsExmple {
@@ -44,7 +47,7 @@ public class AssertionsExmple {
                 .body("query", equalToIgnoringCase("ipod"));
 
     }
-
+    @Ignore
     @Test
     public void hamcrestMatchersHasItem() {
         given()
@@ -57,7 +60,7 @@ public class AssertionsExmple {
                 .body("items.name", hasItem("Apple iPod Nano 7th Generation 16GB Pink, Like New in Retail Packaging"));
 
     }
-
+    @Ignore
     @Test
     public void hamcrestMatchersHasItems() {
         given()
@@ -68,6 +71,33 @@ public class AssertionsExmple {
                 .get("/search")
                 .then()
                 .body("items.name", hasItems("Apple iPod touch 32GB", "Apple iPod touch 128GB", "Apple iPod Touch 6th Generation 32GB Refurbished", "Apple iPod nano 16GB", "Apple iPod touch 16GB", "Apple iPod shuffle 2GB", "Apple iPod touch 32GB (6th Generation - Latest Model)", "Assorted Colors + Bluetooth Speaker + Clear Case + Screen Protector + Stand + Stylus Pen + Cloth", "iPod Touch 6th Generation Case", "iPod Touch 5th Generation Case","ULAK Waterproof Dustproof Sweatproof Cover Built-in Touch Screen with Kickstand for Apple iPod Touch 5th Gen/6th Gen", "7th Generation 16GB iPod Nano Purple-Excellent Condition!", "Apple iPod Touch 5th Generation 16GB Black/Silver Pre-owned Very Good", "ME643LL/A"));
+
+    }
+
+    @Test
+    public void hamcrestMatchersMultipleAssertionsHasKey() {
+        given()
+                .queryParam("query", "ipod")
+                .queryParam("apiKey", APIKEY)
+                .queryParam("format", "json")
+                .when()
+                .get("/search")
+                .then()
+                .body("items[0].giftOptions",hasKey("allowGiftWrap"));
+
+    }
+
+    @Test
+    public void hamcrestMatchersMultipleAssertionsKeyValuePairsInArray() {
+
+        given()
+                .queryParam("query", "ipod")
+                .queryParam("apiKey", APIKEY)
+                .queryParam("format", "json")
+                .when()
+                .get("/search")
+                .then()
+                .body("items.findAll{it.name==~/App.*/}",hasItems(hasEntry("name","Apple iPod touch 32GB")));
 
     }
 
